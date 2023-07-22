9162c3a0b0f0d92153bcf110929748473693754d
manju12345 <manju@gmail.com>
2023-07-22 11:02:08 +0530

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+
+        Console.WriteLine("Hello, " + userName + "!");
+    }
+}
+
